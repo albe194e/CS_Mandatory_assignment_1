59559efed8750fa4e99e4114d633fa33c0a933c6
HaDoCK <jako7096@stud.kea.dk>
2023-09-25 15:18:45 +0200

    update

--- C#Mandatory1/Club.cs
+++ C#Mandatory1/Club.cs
@@ -2,16 +2,16 @@
 
 public class Club
 {
-    public string Name { get; set; }
-    public int GamesPlayed { get; set; }
-    public int GamesWon { get; set; }
-    public int GamesDrawn { get; set; }
-    public int GamesLost { get; set; }
-    public int GoalsFor { get; set; }
-    public int GoalsAgainst { get; set; }
-    public int GoalDifference => GoalsFor - GoalsAgainst;
-    public int Points => (GamesWon * 3) + GamesDrawn;
-    public List<char> WinningStreak { get; set; }
+    private string Name { get; set; }
+    private int GamesPlayed { get; set; }
+    private int GamesWon { get; set; }
+    private int GamesDrawn { get; set; }
+    private int GamesLost { get; set; }
+    private int GoalsFor { get; set; }
+    private int GoalsAgainst { get; set; }
+    private int GoalDifference => GoalsFor - GoalsAgainst;
+    private int Points => (GamesWon * 3) + GamesDrawn;
+    private List<char> WinningStreak { get; set; }
 
     public Club(string name, int gamesPlayed, int gamesWon, int gamesDrawn, int gamesLost, int goalsFor, int goalsAgainst, List<char> winningStreak)
     {
--- C#Mandatory1/Program.cs
+++ C#Mandatory1/Program.cs
@@ -1,8 +1,58 @@
+using System.Globalization;
 using C_Mandatory1;
+using CsvHelper;
+using CsvHelper.Configuration;
+using Microsoft.VisualBasic.FileIO;
 
 public class Program {
+    
+    
      static void Main()
     {
+        string csvFilePath = "C:\\Users\\Nikol\\RiderProjects\\CS_Mandatory_assignment_1\\C#Mandatory1\\Files\\Standings-Nordic.csv";
+
+        // Create a list to store the CSV data
+        List<string[]> csvData = new List<string[]>();
+
+        try
+        {
+            using (StreamReader reader = new StreamReader("Files\\Standings-Nordic.csv"))
+            using (CsvReader csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            using (I)
+            {
+                
+            }
+            {
+                csv.TextFieldType = FieldType.Delimited;
+                csv.SetDelimiters(",");
+
+                // Read and parse the CSV file line by line
+                while (!csv.EndOfData)
+                {
+                    string[] fields = csv.ReadFields();
+                    csvData.Add(fields);
+                }
+            }
+
+            // Now, 'csvData' contains the CSV content as a list of string arrays,
+            // where each string array represents a row of data.
+
+            // You can process the data as needed, e.g., iterate through rows and columns.
+            foreach (var row in csvData)
+            {
+                foreach (var field in row)
+                {
+                    Console.Write($"{field}\t");
+                }
+                Console.WriteLine();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+        
+        /*
         // Load and populate clubs from setup and teams file
         List<Club> clubs = LoadClubsFromFiles(); // Implement this function as per your file format.
 
@@ -19,6 +69,7 @@ public class Program {
 
         // Display the current standings
         DisplayStandings(clubs);
+        */
     }
 
      static void CalculateWinningStreak(List<Club> clubs)
@@ -52,8 +103,43 @@ public class Program {
 
     static List<Club> LoadClubsFromFiles()
     {
-        // Implement code to load clubs from setup and teams file
-        // Return a list of Club objects with the necessary data.
+        string csvFilePath = "Teams-Nordic.csv";
+
+        // Create a list to store the CSV data
+        List<string[]> csvData = new List<string[]>();
+
+        try
+        {
+            using (TextFieldParser parser = new TextFieldParser(csvFilePath))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+
+                // Read and parse the CSV file line by line
+                while (!parser.EndOfData)
+                {
+                    string[] fields = parser.ReadFields();
+                    csvData.Add(fields);
+                }
+            }
+
+            // Now, 'csvData' contains the CSV content as a list of string arrays,
+            // where each string array represents a row of data.
+
+            // You can process the data as needed, e.g., iterate through rows and columns.
+            foreach (var row in csvData)
+            {
+                foreach (var field in row)
+                {
+                    Console.Write($"{field}\t");
+                }
+                Console.WriteLine();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
         return new List<Club>();
     }
 
