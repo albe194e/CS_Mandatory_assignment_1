3e8ec46dc039e30028bae4e780c2006e0cd2ab72
albe194e <albe194e@stud.kea.dk>
2023-09-25 15:56:35 +0200

    FDF

--- C#Mandatory1/FilerHandler.cs
+++ C#Mandatory1/FilerHandler.cs
@@ -1,6 +1,5 @@
 using System.Globalization;
 using C_Mandatory1;
-using CsvHelper;
 using Microsoft.VisualBasic.FileIO;
 
 public class FileHandler {
@@ -42,17 +41,7 @@ public class FileHandler {
     
     public Round ReadRound(string fileName) {
 
-        using StreamReader reader = new StreamReader(fileName + ".csv");
-        using CsvReader csv = new CsvReader(reader, CultureInfo.InvariantCulture);
-        var matches = csv.GetRecords<Match>();
-
-        Round round = new Round();
-
-        foreach (Match match in matches)
-        {
-            round.addMatch(match.HomeTeam, match.AwayTeam, match.Result);
-        }
-
+        
         return round;
     }
 }
