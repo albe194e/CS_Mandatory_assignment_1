509846cc08215fb60d3763309377e4538c8d465a
albe194e <albe194e@stud.kea.dk>
2023-09-27 13:14:56 +0200

    JIVJSD

--- C#Mandatory1/Program.cs
+++ C#Mandatory1/Program.cs
@@ -18,19 +18,51 @@ public class Program {
         
 
         //Super League
-        Console.WriteLine("Super League");
         for (int i = 1; i < format[0] + 1; i++)
         {   
             rh.ProcessRound(League.Super, Part.part1, i);
             rh.ProcessRound(League.Nordic, Part.part1, i);
         }
 
+        /*
         for (int i = 1; i < format[1] + 1; i++)
         {   
             rh.ProcessRound(League.Super, Part.part2, i);
             rh.ProcessRound(League.Nordic, Part.part2, i);
         } 
+        */
 
-        standings.DisplayBy(League.Nordic, Sort.position);
+        bool keepGoing = true;
+        League league = League.Super;
+        while(keepGoing) {
+            
+            Console.WriteLine("\nCurrent League: " + league);
+            Console.WriteLine("Display standings by?" +
+            "\n(0) Change League" +
+            "\n(1) Position" +
+            "\n(2) Games won" +
+            "\n(3) Goals for");
+
+            int input = int.Parse(Console.ReadLine());
+
+            switch (input)
+            {
+                case 0:
+                    league = (league == League.Super) ? League.Nordic : League.Super;
+                    break;
+                case 1:
+                    standings.DisplayBy(league, Sort.position);
+                    break;
+                case 2:
+                    standings.DisplayBy(league, Sort.gamesWon);
+                    break;
+                case 3:
+                    standings.DisplayBy(league, Sort.goalsFor);
+                    break;
+                default:
+                    Console.WriteLine("That sorting is not implemented yet");
+                    break;
+            }
+        }
     }  
 }
